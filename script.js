eb8536c46e2ece3418502db220ebc8eb99190bf5
Paolo Debabsieux <paolo.debaisieux@gmail.com>
2023-04-14 16:34:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.querySelector(".div");
+div.addEventListener("mousemove", (e) => {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  div.style.setProperty("--x", x + "px");
+  div.style.setProperty("--y", y + "px");
+  //   console.log(x);
+  //   console.log(y);
+});
