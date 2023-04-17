8bc7c168886b32e84e9a83221d85ffde1dabc2b0
Paolo Debabsieux <paolo.debaisieux@gmail.com>
2023-04-17 12:57:33 +0200

    hrtr

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ div.addEventListener("mousemove", (e) => {
   let rect = e.target.getBoundingClientRect();
   let x = e.clientX - rect.left;
   let y = e.clientY - rect.top;
-  div.style.setProperty("--x", x + "px");
-  div.style.setProperty("--y", y + "px");
   console.log(x);
   console.log(y);
+  div.style.setProperty("--x", x + "px");
+  div.style.setProperty("--y", y + "px");
 });
